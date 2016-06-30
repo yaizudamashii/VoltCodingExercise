181da96c9ea623d34255fb3b79edaf0df505bd46
Yuki Konda <yuki.konda@gmail.com>
2016-06-30 05:58:26 -0400

    Initial Commit

---
+++ VoltCodingExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoltCodingExercise
+//
+//  Created by Yuki Konda on 6/30/16.
+//  Copyright © 2016 Yuki Konda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
